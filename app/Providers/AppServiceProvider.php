0ba14ed649bd0fc9af5e1de5247aaa0e14914e3b
jordanbardsley7 <jordanbardsley7@yahoo.com>
2017-07-25 18:07:31 -0400

    default schema string length for older versions of mysql/mariadb

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,16 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+    }
+
     public function register()
     {
         foreach ($this->app['config']['repositories'] as $interface => $implementation) {
