94ee423f0f173665c0e60b8bf957e2c31f5e443d
Jordan Bardsley <jordanbardsley7@yahoo.com>
2018-02-16 14:30:21 -0500

    Updated viewport

--- resources/views/templates/auth/app.blade.php
+++ resources/views/templates/auth/app.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <title>{{ config("app.name") }}</title>
         
